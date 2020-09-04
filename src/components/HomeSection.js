2547f5f5726807e5b97f23bdabbe3f65f115812f
shiddarth bista <shiddarthbista@shiddarths-MacBook-Pro.local>
2020-09-04 12:24:57 -0400

    SAD-17: Added login and register components

---
+++ src/components/HomeSection.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HomeSection.css" ;
+
+function HomeSection() {
+  return (
+    <div className='home-container'>
+            
+      <h1>BAMS BARBER SHOP</h1>
+      <p>Come for the freshest trims!!</p>
+      <div className='home-btns'>
+        <Button 
+          className='btns'
+          buttonStye='btn--outline'
+          buttonSize='btn--large'
+          onClick={console.log("hey")}
+        >
+                         BOOK AN APPOINTMENT
+        </Button>
+              
+      </div>
+    </div>
+
+        
+  );
+}
+export default HomeSection;
---
+++ src/components/PriceCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function PriceCard (props) {
+
+  return (
+    <div className="price-card">
+      <h1>{props.haircut.name}</h1>
+      <p>{props.haircut.price } . {props.haircut.time}</p>
+        
+      <hr/>
+    </div>
+  );
+}
+export default PriceCard;
