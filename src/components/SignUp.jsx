d05e877ae466b362f1cd4be143b6c613548d53e6
Shiddarth Bista <shiddarthbista@gmail.com>
2020-09-10 13:56:23 -0400

    SAD-24: Created register input forms
    register components
    
    added css to register components
    
    -made some more css adjustments
    
    fixed h1
    
    css
    
    already have an account
    
    already have acount . login changes
    
    linting fixes
    
    Removed unused images in public
    
    removed unused home component
    
    removed yarn.lock
    
    added dev deps

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "../App.css";
+import "./SignUp.css";
+import { Link } from "react-router-dom";
+
+function SignUp() {
+  return(
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Create Account</h1>
+        <form noValidate >
+          
+          <div className="firstName">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" 
+              className="" 
+              placeholder="First Name" 
+              name="FirstName"
+              noValidate/>
+          </div>
+
+          <div className="lastName">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" 
+              className="" 
+              placeholder="Last Name" 
+              name="LastName"
+              noValidate
+            />
+          </div>
+
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input type="email" 
+              className="" 
+              placeholder="Enter your email address" 
+              name="email"
+              noValidate
+            />
+          </div>
+
+          <div className="phone">
+            <label htmlFor="phone">Phone</label>
+            <input type="tel" 
+              className="" 
+              placeholder="Enter your phone number" 
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              name="phone"
+            
+            />
+          </div>
+
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input type="password" 
+              className="" 
+              placeholder="Enter a password" 
+              name="password"
+              noValidate
+            />
+          </div>
+
+          <div className="createAccount">
+            <button type="submit"> Create an Account</button>
+            <Link to ="/Login" style={{ textDecoration: "none" }}>
+              <small>Already Have an Account? Log in</small>
+            </Link>
+          </div>
+          
+        </form>
+
+      </div>
+    </div>
+  );
+}
+
+
+export default SignUp;
