401fca62a7ceb65bc3fe53637e6d8bd917b3274d
Jim Moua <jimmoua24@gmail.com>
2020-09-11 09:24:35 -0500

    SAD-29: Add Axios module to project (#4)

---
+++ src/components/pages/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css'
+import PriceList from '../PriceList'
+
+function Services(){
+    return(
+        <>
+            <PriceList/>
+        </>
+    );
+}
+
+export default Services;
