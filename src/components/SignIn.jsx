38ee8c817f9f3a4e12c4ab37eb195bc1286b5f2f
Shiddarth Bista <shiddarthbista@gmail.com>
2020-09-13 14:57:15 -0400

    sign in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import "../App.css";
+import "./SignIn.css";
+import { Link } from "react-router-dom";
 
 function SignIn() {
   return(
-    <div>
-      <h1>Login </h1>
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>SIGN IN</h1>
+        <form noValidate  >
+          
+          
+            <div className="email">
+            <label htmlFor="email">Email</label>
+            <input type="email" 
+            className="" 
+            placeholder="Enter your email address" 
+            name="email"
+            noValidate
+            />
+            </div>
+
+           
+
+            <div className="password">
+            <label htmlFor="password">Password</label>
+            <input type="password" 
+            className="" 
+            placeholder="Enter a password" 
+            name="password"
+            noValidate
+            />
+            </div>
+
+            <div className="loginAccount">
+              <button type="submit"> Login</button>
+              <Link to ="/Register" style={{textDecoration: "none"}}>
+              <small>Dont Have an account? Sign up</small>
+              </Link>
+            </div>
+          
+        </form>
+
+      </div>
     </div>
   );
 }
