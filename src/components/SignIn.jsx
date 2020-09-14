952a1439d5480b5352af3e58e6728d05235c31e2
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2020-09-14 10:29:02 -0400

    SAD-25: Login Form
    * sign in
    
    * changed the login button

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "../App.css";
-import {
-  Link
-} from "react-router-dom";
+import "./SignIn.css";
+import { Link } from "react-router-dom";
 
 function SignIn() {
   return(
