f1f14cd4e1374962191ebc14f1aef90d980544c1
Shiddarth Bista <shiddarthbista@gmail.com>
2020-09-24 09:47:20 -0400

    changed password to password 1

--- src/components/validateInfo.jsx
+++ src/components/validateInfo.jsx
@@ -22,10 +22,16 @@ export default function validateInfo(values) {
     errors.phone = "Enter a valid phone number";
   }
   
-  if (!values.password) {
+  if (!values.password1) {
     errors.password = "Password is required";
-  } else if (values.password.length < 6) {
-    errors.password = "Password needs to be 6 characters or more";
+  } else if (values.password1.length < 6) {
+    errors.password1 = "Password needs to be 6 characters or more";
+  }
+
+  if (!values.password2) {
+    errors.password2 = "Password is required";
+  } else if (values.password2 !== values.password1) {
+    errors.password2 = "Passwords do not match";
   }
   
   
