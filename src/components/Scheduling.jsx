f08a8c62c6590d0547494ca7b609a3224b266f29
Shiddarth Bista <shiddarthbista@gmail.com>
2020-09-26 15:53:45 -0400

    Scheduling component

---
+++ src/components/Scheduling.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Scheduling() {
+  return (
+    <div className="schedule">
+            
+      <h1>Scheduling</h1>
+   
+    </div>
+
+        
+  );
+}
+export default Scheduling;
