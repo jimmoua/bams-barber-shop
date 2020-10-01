a572100188daa00273c860f47ab7e86dec57fbcb
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2020-10-01 15:03:48 -0400

    SAD-50: Create an appointment form (#17)
    * change favicon
    
    * Regex for Sign Up forms
    
    * changed pointer
    
    * sign in regex
    
    * linting
    
    * changed to password
    
    * appointment form
    
    * css for appointment form
    
    Co-authored-by: Jim Moua <jimmoua24@gmail.com>

--- src/components/Scheduling.jsx
+++ src/components/Scheduling.jsx
@@ -1,13 +1,50 @@
 import React from "react";
+import "./SignIn.css";
+import "../App.css";
 
-function Scheduling() {
-  return (
-    <div className="schedule">
-            
-      <h1>Scheduling</h1>
-   
+const Scheduling = () => {
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+  };
+
+  return (   
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Schedule an Appointment</h1>
+        <form noValidate onSubmit={handleSubmit}>
+        
+          <div className="date">
+            <label htmlFor="date">Date: </label>
+            <input type="date" 
+              className="" 
+              placeholder="Pick an appointment date" 
+              name="date"
+              noValidate
+            />
+          </div>
+
+          <div className="time">
+            <label htmlFor="time">Time: </label>
+            <input type="time" 
+              className="" 
+              placeholder="Choose a time" 
+              name="time"
+              noValidate
+            />
+         
+          </div>
+
+          <div className="loginAccount">
+            <button type="submit"> Confirm</button>
+          
+          </div>
+        
+        </form>
+
+      </div>
     </div>
         
   );
-}
+};
 export default Scheduling;
