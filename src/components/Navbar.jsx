87a334f6577f652a43f506c8ef5c1a8d46ec4a83
Jim Moua <jimmoua24@gmail.com>
2020-10-01 00:56:44 -0500

    Removed the home link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,11 +37,6 @@ function Navbar() {
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
               <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
                 Services
               </Link>
