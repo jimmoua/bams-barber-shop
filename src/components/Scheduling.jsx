f760b69b483ea50ada6c93337f7778aee1bd0215
Jim Moua <jimmoua24@gmail.com>
2020-10-03 07:05:53 -0500

    changed to functional component

--- src/components/Scheduling.jsx
+++ src/components/Scheduling.jsx
@@ -1,99 +1,122 @@
-import React, { Component } from "react";
+import React from "react";
 import "./SignIn.css";
 import "../App.css";
 import Select from "react-select";
 import axios from "axios";
 import apiUri from "../helpers/apiUri";
 
-export default class Scheduling extends Component {
- 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectOptions : [],
-      id: "",
-      styleName: " ",
-      price: " ",
-      ect: " "
-    };
-  }
+/**
+ * @function Scheduling
+ * 
+ * @description
+ * The scheduling component
+ * 
+ * @returns
+ * React stuff
+ */
+const Scheduling = () => {
 
-  async getOptions() {
-    let data;
-    await axios.get(`${apiUri}/api/styles`)
-      .then(results => {
-        data = results.data;
-      }).catch(err => {
+  const [stylesList, setStylesList] = React.useState([]);
+  const [currStyle, setCurrStyle] = React.useState();
+
+  React.useEffect(() => {
+    const fetchList = async() => {
+      console.log(`${apiUri}/api/styles`);
+      let data;
+      try {
+        data = await (await axios.get(`${apiUri}/api/styles`)).data;
+        setStylesList(data);
+      } catch(err) {
         console.log(err);
-      });
-    
-    const options = data.map(e => ({
-      id: e.id,
-      styleName: e.styleName,
-      price: e.price,
-      ect: e.ect
+      }
+    };
+    fetchList();
+  }, []);
+
+  /**
+   * @function getListOfStyles
+   * 
+   * @returns a data mapping of the styles
+   */
+  const getListOfStyles = () => {
+    return stylesList.map(e => ({
+      value: e.id,
+      label: e.styleName
     }));
+  };
 
-    this.setState({ selectOptions: options });
-  }
-
-  handleChange(e) {
-    this.setState({ id:e.id, styleName:e.styleName, price:e.price, ect:e.ect });
-  }
-
-  componentDidMount() {
-    this.getOptions();
-  }
-
-  async handleSubmit(e) {
-    e.preventDefault();
-  }
-
-  render() {
+  const displayCurrSelectedInfo = () => {
+    if(!currStyle) {
+      return (<React.Fragment></React.Fragment>);
+    }
     return (
-      <div className="wrapper">
-        <div className="form-wrapper">
-          <h1>Schedule an Appointment</h1>
-          <form noValidate onSubmit={this.handleSubmit}>
-
-            <div className="date">
-              <label htmlFor="date">Date: </label>
-              <input type="date"
-                className=""
-                placeholder="Pick an appointment date"
-                name="date"
-                noValidate />
-            </div>
-
-            <div className="time">
-              <label htmlFor="time">Time: </label>
-              <input type="time"
-                className=""
-                placeholder="Choose a time"
-                name="time"
-                min="5:00" max="09:00" required
-                noValidate />
-
-            </div>
-            <br></br>
-
-            <div className="hairstyle">
-              <Select options={this.state.selectOptions} placeholder="Select a hairstyle" onChange={this.handleChange.bind(this)} />
-              <p>Price: $<strong>{this.state.price}</strong></p>
-              <p>Time: <strong>{this.state.ect}</strong> mins</p>
-
-            </div>
-
-            <div className="loginAccount">
-              <button type="submit"> Confirm</button>
-
-            </div>
-
-          </form>
-
-        </div>
-      </div>
-
+      <React.Fragment>
+        <p>Price: $ <strong>{currStyle.price}</strong></p>
+        <p>Time: <strong>{currStyle.ect}</strong> mins</p>
+      </React.Fragment>
     );
-  }
-}
+  };
+ 
+  return (
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Schedule an Appointment</h1>
+        <form noValidate onSubmit={e => e.preventDefault()}>
+
+          <div className="date">
+            <label htmlFor="date">Date: </label>
+            <input
+              type="date"
+              className=""
+              placeholder="Pick an appointment date"
+              name="date"
+              noValidate
+            />
+          </div>
+
+          <div className="time">
+            <label htmlFor="time">Time: </label>
+            <input type="time"
+              className=""
+              placeholder="Choose a time"
+              name="time"
+              min="5:00" max="09:00" required
+              noValidate
+            />
+
+          </div>
+          <br></br>
+
+          <div className="hairstyle">
+            <Select
+              options={getListOfStyles()}
+              placeholder="Select a hairstyle"
+              onChange={ev => {
+                setCurrStyle(stylesList.filter(e => {
+                  return ev.value === e.id;
+                })[0]);
+              }}
+            />
+            {displayCurrSelectedInfo()}
+          </div>
+
+          <div className="loginAccount">
+            <button
+              type="submit"
+              onClick={() => {
+                console.log(currStyle);
+              }}
+            >
+              Confirm
+            </button>
+          </div>
+
+        </form>
+
+      </div>
+    </div>
+  );
+  
+};
+
+export default Scheduling;
