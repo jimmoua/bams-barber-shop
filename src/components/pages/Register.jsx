e567b5d315ee4e4f896497654081639c743bfd91
Jim Moua <jimmoua24@gmail.com>
2020-10-03 11:01:40 -0500

    docs for register component

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -3,16 +3,29 @@ import "../../App.css";
 import SignUp from "../SignUp";
 import FormSuccess from "../FormSuccess";
 
+/**
+ * @function Register
+ * 
+ * @description
+ * Returns the SignUp component if the user has not submitted the form. If the
+ * sign up form has been submitted, return the FormSuccess component.
+ */
 const Register = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
+  /**
+   * @function submitForm
+   * 
+   * @description
+   * Sets the state isSubmit to true
+   */
   function submitForm() {
     setIsSubmitted(true);
   }
+
   return(
     <>
-      {!isSubmitted ? <SignUp submitForm=
-        {submitForm}/> : <FormSuccess/>}
+      { !isSubmitted ? <SignUp submitForm={submitForm}/> : <FormSuccess /> }
     </>
   );
 };
