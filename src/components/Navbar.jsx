a4009c5511c13e62d39a5dc3b47fc036a6c797cd
Shiddarth Bista <shiddarthbista@gmail.com>
2020-10-05 13:53:04 -0400

    Home in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,6 +37,11 @@ function Navbar() {
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
               <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
                 Services
               </Link>
