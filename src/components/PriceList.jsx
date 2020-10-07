c0800b4d8d223a13162d58d79f44cdb69c249fdd
Jim Moua <jimmoua24@gmail.com>
2020-10-06 23:05:38 -0500

    fetch listings from api

--- src/components/PriceList.jsx
+++ src/components/PriceList.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import PriceCard from "./PriceCard";
+import axios from "axios";
+import apiUri from "../api/apiUri";
 
 function PriceList() {
+  const [cardList, setCardList] = React.useState([]);
+  React.useEffect(() => {
+    const fetchList = async() => {
+      await axios.get(`${apiUri}/api/styles`)
+        .then(response => {
+          const list = [];
+          response.data.forEach(e => {
+            list.push(
+              <PriceCard
+                key={e.id}
+                haircut={{
+                  name: e.styleName,
+                  price: e.price,
+                  time: e.ect
+                }}
+              />);
+          });
+          setCardList(list);
+        });
+    };
+    fetchList();
+  }, []);
   return(
     <div className="price">
-      <PriceCard 
-        haircut={{ name: "Haircut + shave", price:"25$", time: "35min" }}
-      />
-      <PriceCard
-        haircut={{
-          name: "Haircut + taper", price:"20$", time: "30min" }}
-      />
-      <PriceCard
-        haircut={{ name: "Drop Fade", price:"15$", time: "25min" }}/>
+      {cardList}
     </div>
   );
 }
