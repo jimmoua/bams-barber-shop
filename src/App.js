4b15b198cc4b8e58d8f4dae5cde6f161f371ea25
Jim Moua <jimmoua24@gmail.com>
2020-10-14 11:27:43 -0500

    SAD-76: Remove register and login component (#24)
    * removed login and register from Navbar
    
    * Removed registe rand login components from main application

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
-import Register from "./components/pages/Register";
-import Login from "./components/pages/Login";
 import { Component404 } from "./components/pages/ErrorPages";
 import Scheduling from "./components/Scheduling/Scheduling";
 
@@ -15,10 +13,8 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/services" exact component={Services}/>
-          <Route path="/login" exact component={Login}/>
-          <Route path="/register" exact component={Register}/>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/services" component={Services}/>
           <Route exact path="/scheduling" component={Scheduling}/>
           <Route component={Component404} />
         </Switch>
