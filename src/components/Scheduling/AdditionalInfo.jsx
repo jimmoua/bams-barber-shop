974ff42b511a4463967be1f9a5d4c498b1699d8d
Shiddarth Bista <shiddarthbista@gmail.com>
2020-10-14 15:52:56 -0400

    Additional info form

--- src/components/Scheduling/AdditionalInfo.jsx
+++ src/components/Scheduling/AdditionalInfo.jsx
@@ -4,10 +4,49 @@ import styles from "../styles/AdditionalInfo.module.css";
 const AdditionalInfo = () => {
   return(
     <React.Fragment>
-      <div className={styles.textarea}>
-        <form action="/form/submit" > 
-          <textarea name="comment" rows="12" cols="35" placeholder="Any Additional Requests"/> <br></br>
-          <input type="submit" name="submitInfo" value="Submit"></input>
+      <div className={styles.infoForm}>
+        <h3>You are nearly done. Enter your details below </h3>
+        <form>
+
+          <div className={styles.phone}>
+            <input type="tel" 
+              className="" 
+              placeholder="Mobile Phone" 
+              name="phone"
+              required
+            />
+          </div>
+
+          <div className={styles.email}>
+            <input type="email" 
+              className="" 
+              placeholder="Email" 
+              name="email"
+              required
+            />
+          </div>
+
+          <div className={styles.firstName}>
+            <input
+              type="text" 
+              placeholder="First Name" 
+              name="firstName"
+              required
+            />
+          </div>
+
+          <div className={styles.lastName}>
+            <input
+              type="text" 
+              placeholder="Last Name" 
+              name="lastName"
+              required
+            />
+          </div>
+
+          <div className={styles.textarea}>
+            <textarea name="comment" rows="5" cols="138" placeholder="Additional notes (optional)"/> 
+          </div>
         </form>
       </div>
     </React.Fragment>
