a617be3e4c06f1faf6e601358fbe91626a8b6978
Shiddarth Bista <shiddarthbista@gmail.com>
2020-10-14 12:51:33 -0400

    deleted opening pages for now

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "../../App.css";
 import HomeSection from "../HomeSection";
-import OpeningHours from "../OpeningHours";
+
 
 function Home() {
   return(
     <>
       <HomeSection/>
-      <OpeningHours/>
     </>
   );
 }
