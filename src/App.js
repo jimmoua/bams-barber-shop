d79e1e9d1eae6cb1154ddf5cfbafe2cb988d3d43
Jim Moua <jimmoua24@gmail.com>
2020-10-16 23:02:25 -0500

    removed Main because 404 component is not routing properly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Main from "./components/Main";
 import Login from "./components/pages/Login";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Scheduling from "./components/Scheduling/Scheduling";
+import Gallery from "./components/Gallery/Gallery";
 import { Component404 } from "./components/pages/ErrorPages";
 import "./App.css";
 
@@ -10,8 +13,11 @@ function App() {
     <>
       <Router>
         <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/services" component={Services}/>
+          <Route exact path="/scheduling" component={Scheduling}/>
+          <Route exact path="/gallery" component={Gallery}/>
           <Route exact path="/login" component={Login} />
-          <Main />
           <Route component={Component404} />
         </Switch>
       </Router>
