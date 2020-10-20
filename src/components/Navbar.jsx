01a1ea5cf2f213ff59da00695cc463574b7c2467
Jim Moua <jimmoua24@gmail.com>
2020-10-20 09:09:18 -0500

    conditionally rendered links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,55 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./styles/Navbar.css";
+import { useStore } from "../store";
 
 function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+  const { state } = useStore();
+
+  /**
+   * @function renderLinks
+   * 
+   * @description
+   * Determines the links to render based on the whether a login state is
+   * present or not.
+   */
+  function renderLinks() {
+    if(state.loggedIn) {
+      return (
+        <React.Fragment>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/gallery" className="nav-links" onClick={closeMobileMenu}>
+                Gallery
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/scheduling" className="nav-links" onClick={closeMobileMenu}>
+                Schedule
+            </Link>
+          </li>
+        </React.Fragment>
+      );
+    }
+  }
 
   return(
     <>
@@ -19,26 +62,7 @@ function Navbar() {
             <i className={click ? "fa fa-times" : "fa fa-bars"}/>
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/gallery" className="nav-links" onClick={closeMobileMenu}>
-                Gallery
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/scheduling" className="nav-links" onClick={closeMobileMenu}>
-                Schedule
-              </Link>
-            </li>
+            {renderLinks()}
           </ul>
         </div>
       </nav>
