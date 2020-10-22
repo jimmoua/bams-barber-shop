dc357c3956bea79843e9f2a6f591df4f88966c53
Jim Moua <jimmoua24@gmail.com>
2020-10-22 14:28:04 -0500

    cliploader loaded on submit

--- src/components/Employee/EditStyle.jsx
+++ src/components/Employee/EditStyle.jsx
@@ -2,12 +2,48 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "../Button";
 import sendUpdate from "../../api/sendUpdate";
+import { ClipLoader } from "react-spinners";
+import { useStore } from "../../store";
+import EditStylesPage from "./EditStylesPage";
 
 /**
  * @param {Object} style The style of the haircut
  */
 const EditStyle = ({ style }) => {
   const [formStyle, setFormStyle] = React.useState(style);
+  const [isSubmitting, setSubmit] = React.useState(false);
+  const { dispatch } = useStore();
+
+  const buttons = () => {
+    if(isSubmitting) {
+      return <ClipLoader />;
+    }
+    return (
+      <React.Fragment>
+        <Button
+          onClick={() => alert("delete c:")}
+        >
+          Delete
+        </Button>
+        <Button
+          onClick={async() => {
+            setSubmit(true);
+            const status = await sendUpdate(formStyle);
+            if(status === 200) {
+              alert(`Style ${formStyle.name} has been updated.`);
+              return dispatch({ type: "setEmployeeComponent", component: <EditStylesPage /> });
+            } else {
+              alert(`Error: style ${formStyle.name} could not be updated.`);
+            }
+            setSubmit(false);
+          }}
+        >
+          Update
+        </Button>
+      </React.Fragment>
+    );
+  };
+
   return(
     <React.Fragment>
       <div>
@@ -24,12 +60,7 @@ const EditStyle = ({ style }) => {
           <input type="text" value={formStyle.time} onChange={ ev=> setFormStyle({ ...formStyle, time: ev.target.value })} />
         </div>
         <div>
-          <Button>Delete</Button>
-          <Button
-            onClick={async() => {
-              await sendUpdate(formStyle);
-            }}
-          >Update</Button>
+          {buttons()}
         </div>
       </div>
     </React.Fragment>
