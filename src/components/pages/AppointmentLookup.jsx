dd69b5d193a05babe75d052015d47079493af1b9
Jim Moua <jimmoua24@gmail.com>
2020-11-05 01:55:44 -0600

    displayed json string for appointment lookup

--- src/components/pages/AppointmentLookup.jsx
+++ src/components/pages/AppointmentLookup.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { appointmentSearchByPhone } from "../../api/appointments";
 
 const AppointmentLookup = () => {
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [appointmentList, setAppointmentList] = React.useState();
+
+  /**
+   * @function lookupButtonHandler
+   * 
+   * @description
+   * Onclick handler submits axios post to backend
+   */
+  const lookupButtonHandler = async() => {
+    const data = await appointmentSearchByPhone(phoneNumber);
+    if(data?.length > 0) {
+      console.log(JSON.stringify(data));
+      setAppointmentList(JSON.stringify(data));
+    } else {
+      setAppointmentList("");
+    }
+  };
   return (
     <React.Fragment>
-      Yup
+      <h1>Appointment Lookup</h1>
+      <form onSubmit={(ev) => ev.preventDefault()}>
+        <input value={phoneNumber} onChange={((ev) => setPhoneNumber(ev.target.value))} type="text"/>
+        <button onClick={lookupButtonHandler}>Lookup</button>
+      </form>
+      <p>{appointmentList}</p>
     </React.Fragment>
   );
 };
