0617cfbaf3f2180c6f646171991ec8bdbe5b7687
Jim Moua <jimmoua24@gmail.com>
2020-11-06 01:35:31 -0600

    added function for appointment cancellation request

--- src/api/appointments.js
+++ src/api/appointments.js
@@ -14,3 +14,23 @@ export async function appointmentSearchByPhone(phoneNumber) {
   }
   return response.data;
 }
+
+/**
+ * @function appointmentCancel
+ * 
+ * @param {String} dateKey The iso 8601 date key used for identifying the appointment
+ * 
+ * @description
+ * Will return http status 200 to denote that a confirmation text was sent to
+ * the user. Otherwise will return something else.
+ */
+export async function appointmentCancel(dateKey) {
+  let response;
+  try {
+    response = await axios.delete(`${apiUri}/api/appointments?dateKey=${dateKey}`, { withCredentials: true });
+  } catch (err) {
+    console.error(err);
+    return err?.response?.status;
+  }
+  return response.status;
+}
