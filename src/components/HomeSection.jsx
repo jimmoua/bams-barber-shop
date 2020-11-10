3d15aed7272beb8784890e377968f970c1918843
Shiddarth Bista <shiddarthbista@gmail.com>
2020-11-10 17:59:46 -0500

    made OpeningHours component

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -21,25 +21,7 @@ function HomeSection() {
           </Button>
         </div>
       </div>
-      <div className={styles.OpeningHourBody}>
-        <div className={styles.sign}>
-          <span className={styles.fastFlicker}>BAMS</span>BARBER<span className={styles.flicker}>SHOP</span>
-        </div>
-        <div className={styles.OpeningTime}>
-          <h1>Opening Hours</h1>
-          <table>
-            <tbody>
-              <tr><th>Sunday: </th><td>Closed</td></tr>
-              <tr><th>Monday: </th><td>Closed</td></tr>
-              <tr><th>Tuesday: </th><td>9am - 6pm</td></tr>
-              <tr><th>Wednesday: </th><td>9am - 6pm</td></tr>
-              <tr><th>Thursday:</th><td>9am - 6pm</td></tr>
-              <tr><th>Friday:</th><td>9am - 6pm</td></tr>
-              <tr><th>Saturday:</th><td>9am - 3pm</td></tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      
     </div>
         
   );
