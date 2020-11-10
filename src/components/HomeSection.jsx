a66916da76d2571f9660fa84f156f84bfe5585cc
Shiddarth Bista <shiddarthbista@gmail.com>
2020-11-10 15:25:45 -0500

    opening hour table

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -25,6 +25,20 @@ function HomeSection() {
         <div className={styles.sign}>
           <span className={styles.fastFlicker}>BAMS</span>BARBER<span className={styles.flicker}>SHOP</span>
         </div>
+        <div className={styles.OpeningTime}>
+          <h1>Opening Hours</h1>
+          <table>
+            <tbody>
+              <tr><th>Sunday: </th><td>Closed</td></tr>
+              <tr><th>Monday: </th><td>Closed</td></tr>
+              <tr><th>Tuesday: </th><td>9am - 6pm</td></tr>
+              <tr><th>Wednesday: </th><td>9am - 6pm</td></tr>
+              <tr><th>Thursday:</th><td>9am - 6pm</td></tr>
+              <tr><th>Friday:</th><td>9am - 6pm</td></tr>
+              <tr><th>Saturday:</th><td>9am - 3pm</td></tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
         
