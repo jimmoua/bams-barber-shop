690cbce03566376899af77588da6c814bdeb13a7
Shiddarth Bista <shiddarthbista@gmail.com>
2020-11-10 13:13:27 -0500

    opening hours

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -5,19 +5,26 @@ import styles from "./styles/HomeSection.module.css" ;
 
 function HomeSection() {
   return (
-    <div className={styles.homeContainer}>
+    <div>
+      <div className={styles.homeContainer}>
             
-      <h1>BAMS BARBER SHOP</h1>
-      <p><i className="fa fa-large fa-phone" aria-hidden="true"></i> :501-318-3142</p>
-      <div className={styles.homeBtns}>
-        <Button 
-          className="btns"
-          buttonStye="btn--outline"
-          buttonSize="btn--large"
-          linkTo="/scheduling"
-        >
+        <h1>BAMS BARBER SHOP</h1>
+        <p><i className="fa fa-large fa-phone" aria-hidden="true"></i> :501-318-3142</p>
+        <div className={styles.homeBtns}>
+          <Button 
+            className="btns"
+            buttonStye="btn--outline"
+            buttonSize="btn--large"
+            linkTo="/scheduling"
+          >
           BOOK AN APPOINTMENT
-        </Button>
+          </Button>
+        </div>
+      </div>
+      <div className={styles.OpeningHourBody}>
+        <div className={styles.sign}>
+          <span className={styles.fastFlicker}>BAMS</span>BARBER<span className={styles.flicker}>SHOP</span>
+        </div>
       </div>
     </div>
         
