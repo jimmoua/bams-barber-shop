bec0a89e44214c85165d2f7ef94ecfca87b8f496
Shiddarth Bista <shiddarthbista@gmail.com>
2020-11-10 19:01:31 -0500

    From the owner section

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -5,23 +5,21 @@ import styles from "./styles/HomeSection.module.css" ;
 
 function HomeSection() {
   return (
-    <div>
-      <div className={styles.homeContainer}>
+
+    <div className={styles.homeContainer}>
             
-        <h1>BAMS BARBER SHOP</h1>
-        <p><i className="fa fa-large fa-phone" aria-hidden="true"></i> :501-318-3142</p>
-        <div className={styles.homeBtns}>
-          <Button 
-            className="btns"
-            buttonStye="btn--outline"
-            buttonSize="btn--large"
-            linkTo="/scheduling"
-          >
+      <h1>BAMS BARBER SHOP</h1>
+      <p><i className="fa fa-large fa-phone" aria-hidden="true"></i> :501-318-3142</p>
+      <div className={styles.homeBtns}>
+        <Button 
+          className="btns"
+          buttonStye="btn--outline"
+          buttonSize="btn--large"
+          linkTo="/scheduling"
+        >
           BOOK AN APPOINTMENT
-          </Button>
-        </div>
+        </Button>
       </div>
-      
     </div>
         
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../App.css";
 import HomeSection from "../HomeSection";
 import OpeningHours from "../OpeningHours";
+import FromOwner from "../FromOwner";
 
 function Home() {
   return(
     <>
       <HomeSection/>
       <OpeningHours/>
+      <FromOwner/>
     </>
   );
 }
