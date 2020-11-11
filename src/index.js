5edb34b52c9ba70135d95fdc724adf4947f87470
Jim Moua <jimmoua24@gmail.com>
2020-11-11 12:13:19 -0600

    Wrapped application around the alert provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {
+  transitions,
+  positions,
+  Provider as AlertProvider
+} from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+// optional configuration
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AlertProvider template={AlertTemplate} options={options}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AlertProvider>,
   document.getElementById("root")
 );
 
