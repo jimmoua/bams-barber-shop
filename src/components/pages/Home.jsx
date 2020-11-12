852129f37d4a0d695aa0cedc089fb14e514cbe8b
Shiddarth Bista <shiddarthbista@gmail.com>
2020-11-12 12:33:24 -0500

    removed from owner componenet

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import "../../App.css";
 import HomeSection from "../HomeSection";
 import OpeningHours from "../OpeningHours";
-import FromOwner from "../FromOwner";
+
 
 function Home() {
   return(
     <>
       <HomeSection/>
       <OpeningHours/>
-      <FromOwner/>
     </>
   );
 }
