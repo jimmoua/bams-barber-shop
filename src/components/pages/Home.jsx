8b60fedd117707cc96e02855bbbfa00f0ceee884
Shiddarth Bista <shiddarthbista@gmail.com>
2020-11-17 16:16:02 -0500

    ImageDisplay in home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../App.css";
 import HomeSection from "../HomeSection";
 import OpeningHours from "../OpeningHours";
+import ImageDisplay from "../ImageDisplay";
 
 function Home() {
   return(
     <>
       <HomeSection/>
       <OpeningHours/>
+      <ImageDisplay/>
     </>
   );
 }
