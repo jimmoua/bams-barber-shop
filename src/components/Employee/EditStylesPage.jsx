db4a98a616dcbbfe887277d1713d0b0ff6bf569c
Shiddarth Bista <shiddarthbista@gmail.com>
2020-11-19 16:17:11 -0500

    added add style buton

--- src/components/Employee/EditStylesPage.jsx
+++ src/components/Employee/EditStylesPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { fetchStyles } from "../../api/styles";
 import ServiceList from "../ServiceList";
+import styles from "../styles/EditStyle.module.css";
 
 const EditStylesPage = () => {
   const [list, setList] = React.useState();
@@ -12,6 +13,9 @@ const EditStylesPage = () => {
   }, []);
   return (
     <React.Fragment>
+      <div className={styles.addButton}>
+        <button> + Add a style</button>
+      </div>
       <ServiceList serviceList={list} displayScheduleNow={false} />
     </React.Fragment>
   );
