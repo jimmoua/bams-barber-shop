352deb0c5820fbfd9f802d5f62e2d10e63e64fa2
Jim Moua <jimmoua24@gmail.com>
2020-11-22 06:43:10 -0600

    fetched and display appointment

--- src/components/Employee/EmployeeHome.jsx
+++ src/components/Employee/EmployeeHome.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { checkStorage } from "../../store";
+import styles from "../styles/EmployeeHome.module.css";
+import ReactDatePicker from "react-datepicker";
+import moment from "moment-timezone";
+import ClipLoader from "react-spinners/ClipLoader";
+import getDay from "date-fns/getDay";
+import { getAppointmentByDate } from "../../api/appointments";
 
 /**
  * @function EmployeeHome
@@ -17,9 +23,53 @@ const EmployeeHome = () => {
     checkStorage();
   });
 
+  const [chosenDate, setChosenDate] = React.useState(moment().toDate());
+  const [loading, setLoading] = React.useState(false);
+  const [appointments, setAppointments] = React.useState();
+
+  const displayData = () => {
+    if(loading) {
+      return <ClipLoader />;
+    }
+    return (
+      <div className={styles.aptListContainer}>{appointments}</div>
+    );
+  };
+
   return (
     <React.Fragment>
-      <h1>Employee Homepage</h1>
+      <div className={styles.container}>
+        <h1>Employee Homepage</h1>
+        <ReactDatePicker
+          placeholderText="Select a Date"
+          selected={chosenDate}
+          onChange={async(date) => {
+            setLoading(true);
+            setChosenDate(date);
+            const apts = await getAppointmentByDate(date);
+            const aptList = [];
+            for(let i = 0; i < apts.length; i++) {
+              aptList.push(
+                <p key={i} className={styles.aptListItem}>Hello</p>
+              );
+            }
+            setAppointments(aptList);
+            setLoading(false);
+          }}
+          // minDate={new Date()}
+          withPortal
+          todayButton="Go to Today"
+          required
+          filterDate={(date) => {
+            // 0 -> Sunday
+            // 1 -> Monday
+            // Return the days that are not these two
+            const day = getDay(date);
+            return day !== 0 && day !== 1;
+          }}
+        />
+        {displayData()}
+      </div>
     </React.Fragment>
   );
 };
