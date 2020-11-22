758d6a82f5aa89fd3a22e2783bae4c4d49cc957d
Jim Moua <jimmoua24@gmail.com>
2020-11-21 21:31:13 -0600

    AddStyle component and added basic validation

--- src/components/Employee/EditStyle.jsx
+++ src/components/Employee/EditStyle.jsx
@@ -35,6 +35,10 @@ const EditStyle = ({ style }) => {
             if(status === 200) {
               alert.success(`Style ${formStyle.name} has been deleted`);
               return dispatch({ type: "setEmployeeComponent", component: <EditStylesPage /> });
+            } else if (status === 409) {
+              alert.error(`Error ${status}: appointments currently exist with that style.`);
+            } else {
+              alert.error("Error: failed to delete the style.");
             }
             setSubmit(false);
           }}
@@ -44,12 +48,21 @@ const EditStyle = ({ style }) => {
         <Button
           buttonStyle="btn--edit"
           onClick={async() => {
+            if(formStyle.name.length === 0) {
+              return alert.show("Please fill out style name.");
+            }
+            if(Number.isNaN(parseFloat(formStyle.price)) || Number.isNaN(formStyle.ect)) {
+              return alert.error("Please make sure that price and estimated time are numbers");
+            }
             setSubmit(true);
             const status = await updateStyle(formStyle);
             if(status === 200) {
               alert.success(`Style ${formStyle.name} has been updated.`);
               return dispatch({ type: "setEmployeeComponent", component: <EditStylesPage /> });
+            } else if (status === 409) {
+              alert.error(`Error ${status}: appointments currently exist with that style.`);
             } else {
+              console.log(status);
               alert.error(`Error: style ${formStyle.name} could not be updated.`);
             }
             setSubmit(false);
@@ -67,15 +80,15 @@ const EditStyle = ({ style }) => {
         <div className={styles.inner}>
           <div className={styles.label}>
             <label>Style name : </label>
-            <input type="text" value={formStyle.name} onChange={ ev => setFormStyle({ ...formStyle, name: ev.target.value }) } />
+            <input required type="text" value={formStyle.name} onChange={ ev => setFormStyle({ ...formStyle, name: ev.target.value }) } />
           </div>
           <div className={styles.label}>
             <label>Price : </label>
-            <input type="text" value={formStyle.price} onChange={ ev => setFormStyle({ ...formStyle, price: ev.target.value })} />
+            <input required type="text" value={formStyle.price} onChange={ ev => setFormStyle({ ...formStyle, price: ev.target.value })} />
           </div>
           <div className={styles.label}>
             <label>Estimated time :</label>
-            <input type="text" value={formStyle.time} onChange={ ev=> setFormStyle({ ...formStyle, time: ev.target.value })} />
+            <input required type="text" value={formStyle.time} onChange={ ev=> setFormStyle({ ...formStyle, time: ev.target.value })} />
           </div>
           <br/>
           <div className={styles.buttons}>
