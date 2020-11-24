6c1756f52487cfe68555d8d5b9ffc0273f64f4b2
Jim Moua <jimmoua24@gmail.com>
2020-11-24 03:17:11 -0600

    added specific appointment view and popups for cancellation

--- src/components/Employee/EmployeeHome.jsx
+++ src/components/Employee/EmployeeHome.jsx
@@ -5,11 +5,13 @@ import ReactDatePicker from "react-datepicker";
 import moment from "moment-timezone";
 import ClipLoader from "react-spinners/ClipLoader";
 import getDay from "date-fns/getDay";
-import { getAppointmentByDate } from "../../api/appointments";
+import { appointmentCancelEmployee, getAppointmentByDate } from "../../api/appointments";
 import MaterialTable from "material-table";
 import Popup from "reactjs-popup";
 import Button from "@material-ui/core/Button";
 import "reactjs-popup/dist/index.css";
+import { useAlert } from "react-alert";
+import DetailView from "./DetailView";
 
 /**
  * @function EmployeeHome
@@ -18,6 +20,12 @@ import "reactjs-popup/dist/index.css";
  * The React component for the home page of an emloyee view
  */
 const EmployeeHome = () => {
+  const [chosenDate, setChosenDate] = React.useState();
+  const [loading, setLoading] = React.useState(false);
+  const [appointments, setAppointments] = React.useState([]);
+  const [popupComponent, setPopupComponent] = React.useState(<React.Fragment></React.Fragment>);
+  const alert = useAlert();
+
   /**
    * Everytime the component mounts, check the storage for the expiration date.
    * Once the local storage logged in state is removed, the component will
@@ -27,13 +35,12 @@ const EmployeeHome = () => {
     checkStorage();
   });
 
-  const [chosenDate, setChosenDate] = React.useState(moment().toDate());
-  const [loading, setLoading] = React.useState(false);
-  const [appointments, setAppointments] = React.useState([]);
-
   const displayData = () => {
     if(loading) {
       return <ClipLoader />;
+    }
+    if(!chosenDate) {
+      return <React.Fragment></React.Fragment>;
     }
     const rowData = [];
     for(let i = 0; i < appointments.length; i++) {
@@ -58,33 +65,70 @@ const EmployeeHome = () => {
      */
     const render = rowData => {
       const { formDetails: fd } = rowData.key;
+      const { service } = rowData.key;
       return (
-        <Popup modal nested open>
-          {close => (
-            <div className={styles.modal}>
-              {/* Close on the top right corner */}
-              <button className={styles.close} onClick={close}>
-                &times;
-              </button>
-              <div className={styles.header}>Additional Information for {fd.firstName} {fd.lastName}</div>
-              <div className={styles.content}>
-                <p style={{ textAlign: "center" }}>
-                  {fd.additionalInfo?.length > 0 ? fd.additionalInfo : "N/A"}
-                </p>
-              </div>
-              <div className={styles.actions}>
-                <Button
-                  className={styles.button}
-                  onClick={() => {
-                    close();
-                  }}
-                >
-                  close
-                </Button>
-              </div>
-            </div>
-          )}
+        <Popup modal nested open closeOnDocumentClick={false} closeOnEscape={false}>
+          {(close) => {
+            return <DetailView close={close} fd={fd} serviceKey={service} />;
+          }}
         </Popup>
+      );
+    };
+
+    /**
+     * @description
+     * cancels the appointment immediately from the backend
+     */
+    const requestCancelAppointment = (date) => {
+      appointmentCancelEmployee(date)
+        .then(status => {
+          if(status === 200) {
+            alert.success("The appointment was canceled.");
+          } else {
+            alert.error("The appointment could not be canceled.");
+          }
+          setPopupComponent(<React.Fragment></React.Fragment>);
+        });
+    };
+
+    const handleCancelBtnPress = (close, rowData) => {
+      const { date } = rowData.key;
+      return (
+        <React.Fragment>
+          <Button
+            className={styles.button}
+            onClick={() => {
+              setPopupComponent(<React.Fragment></React.Fragment>);
+              close();
+            }}
+          >
+            No
+          </Button>
+          <Button
+            className={styles.button}
+            onClick={() => {
+              setPopupComponent(
+                <Popup modal open closeOnDocumentClick={false} closeOnEscape={false}>
+                  {() => {
+                    requestCancelAppointment(date);
+                    return (
+                      <div className={styles.modal}>
+                        <div className={styles.header}>Canceling Appointment...</div>
+                        <div className={styles.content}>
+                          <div style={{ textAlign: "center" }}>
+                            <ClipLoader />
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }}
+                </Popup>
+              );
+            }}
+          >
+            Yes
+          </Button>
+        </React.Fragment>
       );
     };
 
@@ -114,6 +158,35 @@ const EmployeeHome = () => {
             }
           ]}
           onRowClick={(ev, rowData, togglePanel) => {togglePanel();}}
+          actions={[
+            {
+              icon: "clear",
+              tooltip: "Cancel Appointment",
+              onClick: (ev, rowData) => {
+                setPopupComponent(
+                  <Popup open closeOnDocumentClick={false} closeOnEscape={false}>
+                    {(close) => (
+                      <div className={styles.modal}>
+                        <button className={styles.close} onClick={() => {
+                          setPopupComponent(<React.Fragment></React.Fragment>);
+                          close();
+                        }}>&times;</button>
+                        <div className={styles.header}>Cancel Appointment</div>
+                        <div className={styles.content}>
+                          <p style={{ textAlign: "center" }}>
+                            Are you sure you want to cancel the appointment?
+                          </p>
+                        </div>
+                        <div className={styles.actions}>
+                          {handleCancelBtnPress(close, rowData)}
+                        </div>
+                      </div>
+                    )}
+                  </Popup>
+                );
+              }
+            }
+          ]}
         />
       </div>
 
@@ -122,6 +195,7 @@ const EmployeeHome = () => {
 
   return (
     <React.Fragment>
+      {popupComponent}
       <div className={styles.container}>
         <h1>Employee Homepage</h1>
         <ReactDatePicker
